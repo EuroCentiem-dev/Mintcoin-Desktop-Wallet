3177ebc48d5cc5d73c8f08bfb66f2e4363a93eab
EuroCentiem-dev <eurocentiem@mail.com>
2021-06-24 20:09:03 +0200

    changed package order

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -6,7 +6,7 @@ native_packages := native_ccache native_comparisontool
 qt_native_packages = native_protobuf 
 qt_packages = qrencode protobuf zlib
 
-qt_linux_packages= qt freetype fontconfig libxcb libX11 xproto libXext expat dbus libxcb xcb_proto libXau xextproto  xtrans 
+qt_linux_packages= qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
