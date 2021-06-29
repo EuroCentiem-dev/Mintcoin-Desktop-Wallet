4cf789980dbfe4a5b8a668076b8b53d875c4d966
EuroCentiem-dev <eurocentiem@mail.com>
2021-06-29 15:44:59 +0200

    bumped version to 5.1.29

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -1,8 +1,8 @@
 package=bdb
-$(package)_version=5.3.21
+$(package)_version=5.1.29
 $(package)_download_path=http://download.oracle.com/berkeley-db
 $(package)_file_name=db-$($(package)_version).NC.tar.gz
-$(package)_sha256_hash=db4afad0bcb49ed76ac36cc8ded5b1bb893508745a27647c85a858c5d030d5c7
+$(package)_sha256_hash=08238e59736d1aacdd47cfb8e68684c695516c37f4fbe1b8267dde58dc3a576c
 $(package)_build_subdir=build_unix
 
 define $(package)_set_vars
@@ -13,8 +13,10 @@ $(package)_cxxflags=-std=c++11
 endef
 
 define $(package)_preprocess_cmds
-  sed -i.old 's/__atomic_compare_exchange/__atomic_compare_exchange_db/' dbinc/atomic.h && \
-  sed -i.old 's/atomic_init/atomic_init_db/' dbinc/atomic.h mp/mp_region.c mp/mp_mvcc.c mp/mp_fget.c mutex/mut_method.c mutex/mut_tas.c
+  sed -i.old 's/__atomic_compare_exchange/__atomic_compare_exchange_db/' src/dbinc/atomic.h && \
+  sed -i.old 's/atomic_init/atomic_init_db/' src/dbinc/atomic.h src/mp/mp_region.c src/mp/mp_mvcc.c src/mp/mp_fget.c src/mutex/mut_method.c src/mutex/mut_tas.c && \
+  sed -i.old 's/WinIoCtl\.h/winioctl\.h/g' src/dbinc/win_db.h && \
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist
 endef
 
 define $(package)_config_cmds
@@ -22,7 +24,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) libdb_cxx-5.3.a libdb-5.3.a
+  $(MAKE) libdb_cxx-5.1.a libdb-5.1.a
 endef
 
 define $(package)_stage_cmds
