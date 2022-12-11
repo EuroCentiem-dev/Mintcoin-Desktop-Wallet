dcc99fb8a89d3c39414e9107393364516cb8a771
EuroCentiem-dev <eurocentiem@mail.com>
2022-12-11 10:54:40 +0100

    Version bump to 2.5.0 due to vurnabilities

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,8 +1,8 @@
 package=expat
-$(package)_version=2.4.1
+$(package)_version=2.5.0
 $(package)_download_path=http://sourceforge.net/projects/expat/files/expat/$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=a00ae8a6b96b63a3910ddc1100b1a7ef50dc26dceb65ced18ded31ab392f132b
+$(package)_sha256_hash=6b902ab103843592be5e99504f846ec109c1abb692e85347587f237a4ffa1033
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static
