6930be7ad14b839cb67f2d40e7e4cbc36c4d0ee7
EuroCentiem-dev <eurocentiem@mail.com>
2025-09-28 10:48:41 +0000

    Update file permissions and add new package definitions for Berkeley DB versions

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -4,13 +4,21 @@ native_packages := native_ccache
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans 
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
 wallet_packages=bdb
+
+ifeq ($(host_os),linux)
+wallet_packages=bdb_5_3_28
+endif
+
+ifeq ($(host_os),mingw32)
+wallet_packages=bdb_6_1_26
+endif
 
 upnp_packages=miniupnpc
 
